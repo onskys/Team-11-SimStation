0577c9a42465a71c0f028a62eba907a7bb910d12
Owen <104656426+onskys@users.noreply.github.com>
2023-04-06 14:51:32 -0700

    Added prof version

--- src/mvc/AppFactory.java
+++ src/mvc/AppFactory.java
@@ -3,21 +3,16 @@ package mvc;
 /* Class "AppFactory" Datalog
 4/4/2023 - Owen Semersky: Created File
                           Imported Owen's version of AppFactory
+4/6/2023 - Owen Semersky: Added professor's version of AppFactory
 
  */
 
 public interface AppFactory {
-    Model makeModel();
-
-    View makeView(Model m);
-
-    String getTitle();
-
-    String[] getHelp();
-
-    String about();
-
-    String[] getEditCommands();
-
-    Command makeEditCommand(Model model, String name, Object source);
+    public Model makeModel();
+    public View makeView(Model model);
+    public String[] getEditCommands();
+    public Command makeEditCommand(Model model, String type, Object source);
+    public String getTitle();
+    public String[] getHelp();
+    public String about();
 }
