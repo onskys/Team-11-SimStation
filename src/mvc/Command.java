8d0c0eaae5cdfe361fb18f076b981c8c9968e1e2
Owen <104656426+onskys@users.noreply.github.com>
2023-04-06 14:53:09 -0700

    Added prof version

--- src/mvc/Command.java
+++ src/mvc/Command.java
@@ -3,16 +3,18 @@ package mvc;
 /* Class "Command" Datalog
 4/4/2023 - Owen Semersky: Created File
                           Imported Owen's version of Command
+4/6/2023 - Owen Semersky: Added professor's version of Command
 
  */
 
 public abstract class Command {
+    protected Model model;
 
-    public Model model;
-
-    public Command(Model model){
+    public Command(Model model) {
+        super();
         this.model = model;
     }
 
-    public abstract void execute();
+    public abstract void execute() throws Exception ;
+
 }
