b08b5e742f38935796e524b655d9bcd163885315
Owen <104656426+onskys@users.noreply.github.com>
2023-04-07 15:08:11 -0700

    Edits to some methods

--- src/simstation/Simulation.java
+++ src/simstation/Simulation.java
@@ -7,6 +7,7 @@ import java.util.*;
 4/4/2023 - Owen Semersky: Created File
                           Added mvc import and Model extension, added given code
                           Added outline (variables and methods)
+4/7/2023 - Owen Semersky: Made edits to some methods.
 
 
  */
@@ -14,7 +15,7 @@ import java.util.*;
 public class Simulation extends Model {
 
     private Timer timer;
-    private int clock; // clock: int = 0
+    private int clock = 0; // clock: int = 0
 
     private void startTimer() {
         timer = new Timer();
@@ -33,11 +34,29 @@ public class Simulation extends Model {
         }
     }
 
+    // Starts the simulation.
     // start()
+
+    // Pauses the simulation.
     // suspend()
+
+    // Resumes the simulation.
     // resume()
+
+    // Stops the simulation entirely.
     // stop()
-    // getNeighbor(a: Agent, radius: Double): Agent
-    // populate()
+
+    // Gets a nearby neighboring agent in the simulation.
+    public Agent getNeighbor(Agent a, Double radius) {
+        // Code here
+        return null; // Returns nearby agent
+    }
+
+    // Populate is empty, specified in subclasses.
+    public void populate() {
+
+    }
+
+    // Adds an agent to this simulation.
     // addAgent() ???
 }
