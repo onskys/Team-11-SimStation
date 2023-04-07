c49fa16c42537bcc30ac12d8234c562941a9abe3
Owen <104656426+onskys@users.noreply.github.com>
2023-04-07 15:06:17 -0700

    Added some methods/code

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -1,12 +1,26 @@
 package simstation;
 
 import mvc.*;
+import javax.swing.*;
+import java.awt.*;
 
 /* Class "SimulationView" Datalog
 4/4/2023 - Owen Semersky: Created file
+4/7/2023 - Owen Semersky: Added method headers and some basic code
 
  */
 
 public class SimulationView extends View {
 
+    Simulation sim;
+    
+    public SimulationView(Simulation s) {
+        super(s);
+        sim = s;
+        // Continued
+    }
+    
+    public void paintComponent(Graphics gc) { // Unsure if needed
+        
+    }
 }
