85dd534e31160a94cb8fad046b70f934a22a25f9
Owen <104656426+onskys@users.noreply.github.com>
2023-04-09 19:33:23 -0700

    Added headers

--- src/simstation/StatsCommand.java
+++ src/simstation/StatsCommand.java
@@ -5,13 +5,18 @@ import mvc.*;
 /* Class "StatsCommand" Datalog
 4/6/2023 - Owen Semersky: Created file
                           Added outline
+4/9/2023 - Owen Semersky: Added method headers
 
  */
 
 public class StatsCommand extends Command {
-    // Model
+    public StatsCommand(Model model) {
+        super(model);
+        // Continued
+    }
 
-    // Constructor
-
-    // execute()
+    public void execute() {
+        Simulation sim = (Simulation) model;
+        // Continued
+    }
 }
