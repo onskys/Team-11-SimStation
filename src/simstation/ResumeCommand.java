5bf5eddb2b068a6c5ed22f7ac025042b9768a049
Owen <104656426+onskys@users.noreply.github.com>
2023-04-09 19:32:45 -0700

    Added headers

--- src/simstation/ResumeCommand.java
+++ src/simstation/ResumeCommand.java
@@ -5,13 +5,18 @@ import mvc.*;
 /* Class "ResumeCommand" Datalog
 4/6/2023 - Owen Semersky: Created file
                           Added outline
+4/9/2023 - Owen Semersky: Added method headers
 
  */
 
 public class ResumeCommand extends Command {
-    // Model
+    public ResumeCommand(Model model) {
+        super(model);
+        // Continued
+    }
 
-    // Constructor
-
-    // execute()
+    public void execute() {
+        Simulation sim = (Simulation) model;
+        // Continued
+    }
 }
