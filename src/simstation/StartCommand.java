2a5393f4ea8ecf21712c806c2e2ac066664c7e77
Owen <104656426+onskys@users.noreply.github.com>
2023-04-09 19:33:04 -0700

    Added headers

--- src/simstation/StartCommand.java
+++ src/simstation/StartCommand.java
@@ -5,13 +5,18 @@ import mvc.*;
 /* Class "StartCommand" Datalog
 4/6/2023 - Owen Semersky: Created file
                           Added outline
+4/9/2023 - Owen Semersky: Added method headers
 
  */
 
 public class StartCommand extends Command {
-    // Model
+    public StartCommand(Model model) {
+        super(model);
+        // Continued
+    }
 
-    // Constructor
-
-    // execute()
+    public void execute() {
+        Simulation sim = (Simulation) model;
+        // Continued
+    }
 }
