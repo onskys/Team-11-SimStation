48f24eb0ba6efdba1d9a9fcd8e5810f1b7b76982
Owen <104656426+onskys@users.noreply.github.com>
2023-04-12 08:44:21 -0700

    Added Owen's version

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -7,20 +7,25 @@ import java.awt.*;
 /* Class "SimulationView" Datalog
 4/4/2023 - Owen Semersky: Created file
 4/7/2023 - Owen Semersky: Added method headers and some basic code
+4/11/2023 - Owen Semersky: Minor edits
 
  */
 
 public class SimulationView extends View {
 
     Simulation sim;
-    
+
     public SimulationView(Simulation s) {
         super(s);
         sim = s;
+        sim.populate();
         // Continued
     }
-    
+
     public void paintComponent(Graphics gc) { // Unsure if needed
-        
+        super.paintComponent(gc);
+        for (Agent a : sim.getAgents()) {
+            // Draw agent?
+        }
     }
 }
