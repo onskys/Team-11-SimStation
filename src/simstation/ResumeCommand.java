8f35d42940a3ed63542a025a9e6836e7b3264d12
Owen <104656426+onskys@users.noreply.github.com>
2023-04-12 08:42:21 -0700

    Added Owen's version

--- src/simstation/ResumeCommand.java
+++ src/simstation/ResumeCommand.java
@@ -6,6 +6,7 @@ import mvc.*;
 4/6/2023 - Owen Semersky: Created file
                           Added outline
 4/9/2023 - Owen Semersky: Added method headers
+4/11/2023 - Owen Semersky: Made edits to execute
 
  */
 
@@ -17,6 +18,6 @@ public class ResumeCommand extends Command {
 
     public void execute() {
         Simulation sim = (Simulation) model;
-        // Continued
+        sim.resume();
     }
 }
