06cc206c7d0ab9467843e4ad810d5d77f71f6ab4
Owen <104656426+onskys@users.noreply.github.com>
2023-04-12 08:41:40 -0700

    Adding Owen's version

--- src/simstation/Agent.java
+++ src/simstation/Agent.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
                           Added implementation for move method
 4/9/2023 - Owen Semersky: Made edits to methods
                           Changed class and update method to be abstract
+4/11/2023 - Owen Semersky: Implemented some methods, added some methods
  */
 
 public abstract class Agent implements Serializable, Runnable {
@@ -18,19 +19,50 @@ public abstract class Agent implements Serializable, Runnable {
     protected Heading heading;
     private int xc;
     private int yc;
-    private boolean suspended = false;
-    private boolean stopped = false;
+    private boolean suspended;
+    private boolean stopped;
     private Thread myThread;
+    private Simulation world;
+
+    // Constructor
+    public Agent (String name) {
+        this.name = name;
+        suspended = false;
+        stopped = false;
+        myThread = null;
+    }
 
     // Run method, active agent movement or change.
     public void run() {
-        while (suspended == false && stopped == false) {
-            update();
+        myThread = new Thread(this);
+        myThread.start();
+        while (!isStopped()) {
+            try {
+                // System.out.println("We have a problem");
+                update();
+                Thread.sleep(20);
+                checkSuspended();
+            }
+            catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private synchronized void checkSuspended() {
+        try {
+            while(!stopped && suspended) {
+                wait();
+                suspended = false;
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 
     // Start method, starts running of the agent.
     public void start() {
+        System.out.println("Starting");
         suspended = false;
         run();
     }
@@ -41,9 +73,9 @@ public abstract class Agent implements Serializable, Runnable {
     }
 
     // Resume method, resumes running of the agent.
-    public void resume() {
-        suspended = false;
-        run();
+    public synchronized void resume() {
+        // suspended = false;
+        notify();
     }
 
     // Stop method, stops all activity of the agent.
@@ -51,11 +83,20 @@ public abstract class Agent implements Serializable, Runnable {
         stopped = true;
     }
 
+    public synchronized boolean isStopped() {
+        return stopped;
+    }
+
+    public synchronized boolean isSuspended() {
+        return suspended;
+    }
+
     // Update method, updates current qualities of agent, such as position.
     public abstract void update();
 
     // Moves the Agent in a direction depending on their heading. 8 cases.
     public void move(int steps) {
+        System.out.println("We're moving!");
         if (heading == Heading.NORTH) {
             yc = yc - steps;
         }
@@ -84,5 +125,23 @@ public abstract class Agent implements Serializable, Runnable {
             yc = yc - steps;
             xc = xc - steps;
         }
+        world.changed();
+        // notify();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public synchronized String toString() {
+        String result = name;
+        if (stopped) result += "(stopped)";
+        else if (suspended) result += " (suspended)";
+        else result += " (running)";
+        return result;
+    }
+
+    public void setWorld(Simulation s) {
+        this.world = s;
     }
 }
