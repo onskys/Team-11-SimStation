0c69d775371685164f5a3aed038f09d99c4bc59d
Owen <104656426+onskys@users.noreply.github.com>
2023-04-13 10:42:06 -0700

    Adding Sanjana's version

--- src/simstation/SuspendCommand.java
+++ src/simstation/SuspendCommand.java
@@ -6,17 +6,16 @@ import mvc.*;
 4/6/2023 - Owen Semersky: Created file
                           Added outline
 4/9/2023 - Owen Semersky: Added method headers
-
+4/9/2023 - Sanjana Jagarlapudi: continued implementation of execute method.
  */
 
 public class SuspendCommand extends Command {
     public SuspendCommand(Model model) {
         super(model);
-        // Continued
     }
 
     public void execute() {
         Simulation sim = (Simulation) model;
-        // Continued
+        sim.suspend();
     }
 }
