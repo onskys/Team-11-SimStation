4c2414ed7dafd6659318eda8f29f05738ab97e5f
Owen <104656426+onskys@users.noreply.github.com>
2023-04-13 10:39:07 -0700

    Adding Sanjana's version

--- src/simstation/StartCommand.java
+++ src/simstation/StartCommand.java
@@ -6,17 +6,16 @@ import mvc.*;
 4/6/2023 - Owen Semersky: Created file
                           Added outline
 4/9/2023 - Owen Semersky: Added method headers
-
+4/9/2023 - Sanjana Jagarlapudi: continued implementation of execute method.
  */
 
 public class StartCommand extends Command {
     public StartCommand(Model model) {
         super(model);
-        // Continued
     }
 
     public void execute() {
         Simulation sim = (Simulation) model;
-        // Continued
+        sim.start();
     }
 }
