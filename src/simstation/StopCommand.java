8b7a0259c06a5e8ee557e15b66d236ffe8f43f37
Owen <104656426+onskys@users.noreply.github.com>
2023-04-13 10:40:19 -0700

    Adding Sanjana's version

--- src/simstation/StopCommand.java
+++ src/simstation/StopCommand.java
@@ -6,17 +6,16 @@ import mvc.*;
 4/6/2023 - Owen Semersky: Created file
                           Added outline
 4/9/2023 - Owen Semersky: Added method headers
-
+4/9/2023 - Sanjana Jagarlapudi: continued implementation of execute method.
  */
 
 public class StopCommand extends Command {
     public StopCommand(Model model) {
         super(model);
-        // Continued
     }
 
     public void execute() {
         Simulation sim = (Simulation) model;
-        // Continued
+        sim.stop();
     }
 }
